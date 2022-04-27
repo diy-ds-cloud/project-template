90ff23bc7258360a6e34dec865fec5160345d30e
Sang-Yun Oh <syoh@ucsb.edu>
2022-04-27 18:43:44 +0000

    rename deploy scripts

--- r-future/{{ cookiecutter.github_reponame }}/{{ cookiecutter.__diy-project-name }}-deploy.sh
+++ r-future/{{ cookiecutter.github_reponame }}/{{ cookiecutter.__diy-project-name }}-deploy.sh
@@ -7,21 +7,28 @@
 
 set -e
 
+DIY_CONFIG_DIR="${HOME}/.config/{{ cookiecutter.__diy-project-name }}"
+DIY_CONFIG_FILE="${DIY_CONFIG_DIR}/config.sh"
+
+if [[ -f $DIY_CONFIG_FILE ]]; then
+    source $DIY_CONFIG_FILE
+else
+    echo "Run {{ cookiecutter.__diy-project-name }}-setup.sh first before running this script"
+    exit 1
+fi
+
 echo "--------------------------------------------"
 echo "DIY Cloud Computing for Data Science using R"
 echo "--------------------------------------------"
 
-echo -e "\n\n## Step 1: logging in as user, ${USER_ID}"
-gcloud auth login --no-browser --update-adc
-
-echo -e "\n\n## Step 2: creating cluster under project, ${PROJECT_ID} in region, ${ZONE_CODE}"
+echo -e "\n\n## Creating cluster under project, ${PROJECT_ID} in region, ${ZONE_CODE}"
 terraform -chdir=cluster init
 terraform -chdir=cluster apply -var="project_id=${PROJECT_ID}" -var="region=${ZONE_CODE}" -auto-approve
 
 CLUSTER_NAME=$(terraform -chdir=cluster output -raw kubernetes_cluster_name)
 gcloud container clusters get-credentials ${CLUSTER_NAME} --zone=${ZONE_CODE} --project=${PROJECT_ID}
 
-echo -e "\n\n## Step 3: deploying Rstudio application with Helm chart version, ${HELM_CHART_VERSION}"
+echo -e "\n\n## Deploying Rstudio application with Helm chart version, ${HELM_CHART_VERSION}"
 helm repo add {{ cookiecutter.github_reponame }} https://{{ cookiecutter.github_orgname }}.github.io/{{ cookiecutter.github_reponame }}
 helm repo update
 helm install myrelease {{ cookiecutter.github_reponame }}/{{ cookiecutter.__diy-project-name }} --version "${HELM_CHART_VERSION}" --wait
